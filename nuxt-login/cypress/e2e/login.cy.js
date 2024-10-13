98cf22fa61c112050251b0aadfba537ea74ddf2d
mentesnot-2 <sibhatmentesnot@gmail.com>
2024-10-13 23:33:13 +0300

    added social login test

--- nuxt-login/cypress/e2e/login.cy.js
+++ nuxt-login/cypress/e2e/login.cy.js
@@ -15,30 +15,34 @@ describe('Login Form', () => {
       cy.get('[data-test="submit-btn"]').click();
       cy.get('[data-test="error-email"]').should('contain.text', 'Please enter a valid email');
     });
-  
-    // it('should redirect to signup page on valid email submission', () => {
-    //   const validEmail = 'test@example.com';
-    //   cy.get('[data-test="email-input"]').type(validEmail);
-    //   cy.get('[data-test="submit-btn"]').click();
-    //   cy.url({ timeout: 10000 }).should('include', '/SignupPage');
-    //   cy.url().should('include', `email=${encodeURIComponent(validEmail)}`);
-    // });
-  
-    // it('should navigate to dashboard on Google login click', () => {
-    //   cy.get('[data-test="google-login"]').click();
-    //   cy.url({ timeout: 10000 }).should('include', '/dashboard'); // Increase timeout
-    //   cy.get('[data-test="welcome-text"]').should('contain', 'Welcome to Aladia');
-    // });
-      // it('should navigate to dashboard on Facebook login click', () => {
-      //   cy.get('[data-test="facebook-login"]').click();
-      //   cy.url().should('include', '/dashboard');
-      //   cy.get('[data-test="welcome-text"]').should('contain', 'Welcome to Aladia');
-      // });
     
-      // it('should navigate to dashboard on Apple login click', () => {
-      //   cy.get('[data-test="apple-login"]').click();
-      //   cy.url().should('include', '/dashboard');
-      //   cy.get('[data-test="welcome-text"]').should('contain', 'Welcome to Aladia');
-      // });
+    it('should redirect to signup page on valid email submission', () => {
+      const validEmail = 'test@example.com';
+      cy.get('[data-test="email-input"]').type(validEmail);
+      cy.get('[data-test="submit-btn"]').click();
+      cy.wait(3000);
+      cy.url().should('include', '/SignupPage')
+      cy.url().should('include', `email=${encodeURIComponent(validEmail)}`);
+    });
+  
+    it('should navigate to dashboard on Google login click', () => {
+      cy.get('[data-test="google-login"]').click();
+      cy.wait(3000);
+      cy.url({ timeout: 10000 }).should('include', '/dashboard'); // Increase timeout
+      cy.get('[data-test="welcome-text"]').should('contain', 'Welcome to Aladia');
+    });
+      it('should navigate to dashboard on Facebook login click', () => {
+        cy.get('[data-test="facebook-login"]').click();
+        cy.wait(3000);
+        cy.url().should('include', '/dashboard');
+        cy.get('[data-test="welcome-text"]').should('contain', 'Welcome to Aladia');
+      });
+    
+      it('should navigate to dashboard on Apple login click', () => {
+        cy.get('[data-test="apple-login"]').click();
+        cy.wait(3000);
+        cy.url().should('include', '/dashboard');
+        cy.get('[data-test="welcome-text"]').should('contain', 'Welcome to Aladia');
+      });
   });
   
